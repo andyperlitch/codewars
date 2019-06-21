433e20d5bf9a846cfb2ef573f82e7da7c71dd2a0
Andy Perlitch <andyperlitch@gmail.com>
2019-06-20 22:29:18 -0700

    easy-balance-checking

--- easy-balance-checking.js
+++ easy-balance-checking.js
@@ -1,7 +1,9 @@
+const INVALID_CHARACTERS = /[^a-z\d\.\s]/gi
+
 function balance(book) {
   // Parse input
   const [balanceRaw, ...entriesRaw] = book.split('\n')
-  const balance = Number(balanceRaw)
+  const balance = Number(balanceRaw.replace(INVALID_CHARACTERS, ''))
 
   // Set up accumulators
   let remainingBalance = balance
@@ -10,18 +12,22 @@ function balance(book) {
   // Parse entries
   const entries = entriesRaw
     // Filter empty rows (could do more validation here)
-    .filter((rawEntry) => rawEntry.trim().length > 0)
+    .filter(rawEntry => rawEntry.trim().length > 0)
     // Parse each entry and update accumulators, and return string to be used in report
-    .map((rawEntry) => {
+    .map(rawEntry => {
       // Clean entry
       const cleanedEntry = rawEntry
         // Remove unexpected characters
-        .replace(/[^a-z\d\.\s]/ig, '')
+        .replace(INVALID_CHARACTERS, '')
         // de-dupe spaces
         .replace(/\s+/, ' ')
         // remove outside spaces
         .trim()
-      const [checkNumber, category, checkAmountRaw] = cleanedEntry.trim().split(' ')
+
+      // Parse entry
+      const [checkNumber, category, checkAmountRaw] = cleanedEntry
+        .trim()
+        .split(' ')
       const checkAmount = Number(checkAmountRaw)
 
       // Update accumulators
@@ -32,20 +38,21 @@ function balance(book) {
       // create the report string later
 
       // Return the report string
-      return `${checkNumber} ${category} ${checkAmount} Balance ${remainingBalance.toFixed(2)}`
+      return `${checkNumber} ${category} ${checkAmount.toFixed(
+        2,
+      )} Balance ${remainingBalance.toFixed(2)}`
     })
-    .join('\r\n')
+
+  const entriesString = entries.join('\r\n')
 
   // Calculate the average expense
   const avgExpense = totalExpense / entries.length
-  
-  console.log('totalExpense', totalExpense)
-  
+
   // Generate report
   return [
     `Original Balance: ${balance.toFixed(2)}`,
-    entries,
+    entriesString,
     `Total expense  ${totalExpense.toFixed(2)}`,
-    `Average expense ${avgExpense.toFixed(2)}`
+    `Average expense  ${avgExpense.toFixed(2)}`,
   ].join('\r\n')
 }
